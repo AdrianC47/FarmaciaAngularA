a873a70b75ec77e0b0094bfbea6fea71a9ba8a89
Adrian Cabrera <59811924+AdrianC47@users.noreply.github.com>
2022-08-05 09:17:10 -0500

    Update usuario.component.ts

--- src/app/Componentes/Login/usuario.component.ts
+++ src/app/Componentes/Login/usuario.component.ts
@@ -16,7 +16,7 @@ export class UsuarioComponent implements OnInit {
 
   ngOnInit(): void {
    
-    this.link = "https://farmaciasama.azurewebsites.net/Sistema_Farmacia_S-0.0.1-SNAPSHOT";
+    this.link = "https://farmaciasama.azurewebsites.net/Sistema_Farmacia_S-0.0.1-SNAPSHOT/";
     sessionStorage.setItem("Link",this.link);
   }
 
